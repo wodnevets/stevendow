da006acb246fdcda59a83b1cfb15178ed874ae85
Steven A. R. Dow <hello@wodnevets.com>
2021-09-30 17:19:37 +0200

    Add files via upload

---
+++ scripts/fade-in.js
@@ -0,0 +1,5 @@
+document.body.classList.add('fade');
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    document.body.classList.remove('fade');
+  });
---
+++ scripts/page-load-animation.js
@@ -0,0 +1,4 @@
+import { TweenMax } from 'gsap'
+
+TweenMax.from(".card-bg", .5, { delay: .1, scale: .4, transformOrigin: 'center'})
+
